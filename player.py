19d07c4931fc7388f1b440bdbaa8cf3f71ede50c
Tryny8 <vitry.pro.a@gmail.com>
2025-06-07 20:40:36 +0200

    Moving: Add key forward and backward

--- player.py
+++ player.py
@@ -18,6 +18,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
         
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
         
@@ -25,8 +29,14 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_q]:
-            # ?
+            # Key left
             self.rotate(-dt)
         if keys[pygame.K_d]:
-            # ?
+            # Key right
             self.rotate(dt)
+        if keys[pygame.K_z]:
+            # Key forward
+            self.move(dt)
+        if keys[pygame.K_s]:
+            # Key backward
+            self.move(-dt)
