d2cec857b78314430862430612066f0be192b5a2
Tryny8 <vitry.pro.a@gmail.com>
2025-06-07 16:56:35 +0200

    FPS: Create clock and dt value for FPS limitation

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from constants import *
 
 
 def main():
+   # Init window
    pygame.init()
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
    print("Starting Asteroids!")
    print(f"Screen width: {SCREEN_WIDTH}")
    print(f"Screen height: {SCREEN_HEIGHT}")
+   
+   # Init FPS
+   clock = pygame.time.Clock()
+   dt = 0
    
    while True:
       for event in pygame.event.get():
@@ -16,6 +21,9 @@ def main():
       
       screen.fill("black")
       pygame.display.flip()
+      
+      # limit the framerate to 60 FPS
+      dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
