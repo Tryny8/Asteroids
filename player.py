c9b64d81aecc73361a6a63f23f30ae4340b49174
Tryny8 <vitry.pro.a@gmail.com>
2025-06-08 18:07:15 +0200

    Correction Code

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.shoot_countdown = 0
+        self.shoot_timer = 0
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,17 +26,17 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
-        if self.shoot_countdown > 0:
-            return None
+        if self.shoot_timer > 0:
+            return 
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        self.shoot_countdown = PLAYER_SHOOT_COOLDOWN
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
         
     def update(self, dt):
-        self.shoot_countdown -= dt
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_q]:
